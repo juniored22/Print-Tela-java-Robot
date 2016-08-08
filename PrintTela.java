8e74cc00469a9e1ccd95f5ed9a2008bd681811e7
juniored22 <jo42ap19.11@gmail.com>
2016-08-08 11:17:08 -0300

    Add files via upload

---
+++ PrintTela.java
@@ -0,0 +1,46 @@
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+ 
+
+class PrintTela {
+ 
+    public static void main(String[] args) {
+        try {
+
+
+		
+
+            Robot robot = new Robot();
+            String format = "png";
+            String fileName = "PrintScreenshot." + format;
+	    
+
+	    /*configurando o tamanho da tela
+    	    int x = 100;
+    	    int y = 100;
+            int width = 200;
+            int height = 200;
+            Rectangle telaDesktop = new Rectangle(x, y, width, height);
+            */
+
+	    //capiturando o tamanho da tela
+              Rectangle telaDesktop = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+	    //
+
+	
+            BufferedImage screenFullImage = robot.createScreenCapture(telaDesktop);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+             
+            System.out.println("Imagem salva!!!");
+
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
